26ef5b4adc1625a6ad2428ef9add160e1e7b7f29
enochdev2 <enochthechristepistle@gmail.com>
2024-07-23 09:42:45 +0100

    first commit

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+// import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), 
+  //   sentryVitePlugin({
+  //   org: "Enoch",
+  //   project: "javascript-react"
+  // })
+],
+
+  build: {
+    sourcemap: true
+  }
+})
