2056253af9f042b1786f676a6cfdd2923d5f8be9
enochdev2 <enochthechristepistle@gmail.com>
2024-07-26 20:22:45 +0100

    implement React lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { lazy, Suspense } from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Highlights from './components/Highlights';
@@ -5,6 +6,11 @@ import Model from './components/Model';
 import Features from './components/Features';
 import HowItWorks from './components/HowItWorks';
 import Footer from './components/Footer';
+
+
+const Models = React.lazy(() => import('./components/Model'));
+const  HowItWork = React.lazy(()=> import("./components/HowItWorks"));
+const  Feature = React.lazy(()=> import("./components/Features"));
 
 // import * as Sentry from '@sentry/react';
 
@@ -14,9 +20,19 @@ const App = () => {
       <Navbar />
       <Hero />
       <Highlights />
-      <Model />
-      <Features />
-      <HowItWorks />
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <Models />
+      </Suspense>
+
+      <Suspense fallback={<div>Loading...</div>}>
+      <Feature />
+      </Suspense>
+
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <HowItWork />
+      </Suspense>
       <Footer />
     </main>
   )
